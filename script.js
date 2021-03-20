a97ba69eca8aaff71d09bbb8d65e7984f59e34e0
gentlekboy <gentlekboy@gmail.com>
2021-03-20 21:39:24 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener('click', function () {
+    let showNavLinks = document.getElementById('navbarLinks');
+    showNavLinks.classList.toggle("show");
+})
